cff28d4c94c5533555d8aba9b0955b7e2e3bd3f4
llds <858400061@qq.com>
2025-04-18 21:11:08 +0800

    feat: add router to logo for navigation

--- src/layouts/components/Header/Logo.vue
+++ src/layouts/components/Header/Logo.vue
@@ -1,9 +1,11 @@
 <script setup lang='ts'>
+import { useRouter } from "vue-router";
 
+const router = useRouter()
 </script>
 
 <template>
-    <div class="flex items-center gap-x-4 justify-center md:justify-start">
+    <div @click="router.push('/')" class="flex items-center gap-x-4 justify-center md:justify-start cursor-pointer">
         <img src="/public/logo.svg" class="h-6 w-6">
         <span class="font-700">product-ui-template</span>
     </div>
