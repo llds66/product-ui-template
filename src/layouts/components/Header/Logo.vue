6905fabe46032e1bd3bf1419ddd83767df77df78
llds <858400061@qq.com>
2025-04-19 22:29:03 +0800

    🐞 fix:
    修复 logo 路径错误，改为 /logo.svg

--- src/layouts/components/Header/Logo.vue
+++ src/layouts/components/Header/Logo.vue
@@ -6,7 +6,7 @@ const router = useRouter()
 
 <template>
     <div @click="router.push('/')" class="flex items-center gap-x-4 justify-center md:justify-start cursor-pointer">
-        <img src="/public/logo.svg" class="h-6 w-6">
+        <img src="/logo.svg" class="h-6 w-6">
         <span class="font-700">product-ui-template</span>
     </div>
 </template>
