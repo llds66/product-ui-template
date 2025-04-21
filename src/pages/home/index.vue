102e1f5d88381b8944a57a01877ff1f48a466b01
llds <858400061@qq.com>
2025-04-21 10:22:51 +0800

    feat: add features list and animations for home page

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,46 @@
 <script setup lang='ts'>
+import { ref } from "vue";
 import { motion } from "motion-v"
 import { useRouter } from "vue-router";
 const router = useRouter()
+const featuresList = ref([
+    {
+        icon: 'i-logos-vitejs',
+        title: "Vue 3",
+        description: "Vue 3 is a progressive JavaScript framework used for building user interfaces.",
+    },
+    {
+        icon: 'i-logos-unocss',
+        title: "UnoCSS",
+        description: "UnoCSS is a utility-first CSS framework that provides a fast and efficient way to style your web applications.",
+    },
+    {
+        icon: 'i-logos-vitejs',
+        title: "Motion",
+        description: "Motion is a Vue 3 animation library that provides a simple and powerful way to animate your components.",
+    },
+    {
+        icon: 'i-logos-vitejs',
+        title: "Vite",
+        description: "Vue 3 is a progressive JavaScript framework used for building user interfaces.",
+    },
+    {
+        icon: 'i-logos-vitejs',
+        title: "PrimeVue",
+        description: "UnoCSS is a utility-first CSS framework that provides a fast and efficient way to style your web applications.",
+    },
+    {
+        icon: 'i-logos-vitejs',
+        title: "Lenis",
+        description: "Motion is a Vue 3 animation library that provides a simple and powerful way to animate your components.",
+    },
+])
+
 </script>
 
 <template>
     <div>
+        <!-- #1:主副标题 -->
         <div class="text-3xl md:text-6xl font-bold text-stone-950 dark:text-white  text-center">
             <motion.div :initial="{ opacity: 0, scale: 0.5 }" :animate="{ opacity: 1, scale: 1 }" :transition="{
                 duration: 0.8,
@@ -18,13 +53,15 @@ const router = useRouter()
                 ease: [0, 0.71, 0.2, 1.01]
             }">Website Template.</motion.div>
         </div>
-        <div class="text-lg sm:text-xl text-center max-w-[750px] text-center mx-auto text-stone-500 dark:text-stone-400  mt-5">
+        <div
+            class="text-lg sm:text-xl text-center max-w-[750px] text-center mx-auto text-stone-500 dark:text-stone-400  mt-5">
             <motion.div :initial="{ opacity: 0, scale: 0.5 }" :animate="{ opacity: 1, scale: 1 }" :transition="{
                 duration: 0.8,
                 delay: 0.5,
                 ease: [0, 0.71, 0.2, 1.01]
             }">Modern Vue 3 template styled with UnoCSS and animated with Motion. Lightweight. Flexible.</motion.div>
         </div>
+        <!-- #2:按钮 -->
         <div class="flex justify-center mt-10">
             <motion.button @click="router.push('/effect')" :while-hover="{ scale: 1.1 }" :while-press="{ scale: 0.95 }"
                 :transition="{ type: 'spring', stiffness: 300 }"
@@ -32,8 +69,22 @@ const router = useRouter()
                 <div class="text-white dark:text-stone-950 text-sm">Get Started</div>
             </motion.button>
         </div>
+        <!-- #3:iframe -->
         <div class="rounded-lg shadow-md mt-5 overflow-hidden">
             <iframe src="https://vite.dev/" frameborder="0" height="850" width="100%" scrolling="no"></iframe>
         </div>
+        <!-- #4 -->
+        <motion.div :initial="{ opacity: 0, y: 100 }" :while-in-view="{ opacity: 1, y: 0 }"
+            :transition="{ duration: 0.5, ease: [0.42, 0, 0.58, 1] }"
+            class="mt-10 w-full grid gap-6 cols-1 sm:cols-2 lg:cols-3">
+            <div v-for="i in featuresList" :key="i.title"
+                class="flex items-center gap-x-5 h-30 bg-[rgba(183,183,255,0.05)] border-1 border-solid border-#E5E7EBFF dark:border-#3F3F46 rounded-lg transition-shadow p-5">
+                <div :class="i.icon + ' text-4xl shrink-0'"></div>
+                <div class="flex flex-col">
+                    <div class="text-lg font-bold text-stone-950 dark:text-white">{{ i.title }}</div>
+                    <div class="text-sm text-stone-500 dark:text-stone-400">{{ i.description }}</div>
+                </div>
+            </div>
+        </motion.div>
     </div>
 </template>
